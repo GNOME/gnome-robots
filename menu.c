1907e4638fdc1a0309846cb926e7ff66be9303b4
Alex Duggan <aldug@gnome.org>
2003-01-09 15:52:28 +0000

    Use aisleriot's logo in it's about box, fix small memory leaks in about
    2003-01-08  Alex Duggan  <aldug@gnome.org>
    
    	* aisleriot/menu.c, gataxx/gataxx.c,
    	glines/glines.c, gnibbles/main.c,
    	gnobots2/menu.c, gnome-stones/main.c,
    	gnometris/tetris.cpp, gnomine/gnomine.c,
    	gnotravex/gnotravex.c, gnotski/gnotski.c,
    	gtali/gyahtzee.c, iagno/gnothello.c,
    	mahjongg/mahjongg.c, same-gnome/same-gnome.c:
    	Use aisleriot's logo in it's about box, fix small memory
    	leaks in about boxes when not unref()'ing the pixbuf.

--- menu.c
+++ menu.c
@@ -233,6 +233,9 @@ gpointer data
                           (const char **)documenters,
                           strcmp (translator_credits, "translator_credits") != 0 ? translator_credits : NULL,
                           pixbuf);
+  if (pixbuf != NULL)
+    gdk_pixbuf_unref (pixbuf);
+	
   gtk_window_set_transient_for (GTK_WINDOW (about), GTK_WINDOW(app));
   g_signal_connect (G_OBJECT(about), "destroy", G_CALLBACK(gtk_widget_destroyed), &about);
 

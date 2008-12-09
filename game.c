f8caa22af7fd540d4ac8ea675463857c988c8be6
Robert Ancell <rancell@src.gnome.org>
2008-12-09 13:22:11 +0000

    Use different message for getting the top score versus getting a score in the top ten (Bug #561423)
    svn path=/trunk/; revision=8361

--- game.c
+++ game.c
@@ -180,6 +180,7 @@ show_scores (gint pos, gboolean endofgame)
 				       pos);
       message = g_strdup_printf ("<b>%s</b>\n\n%s",
 				 _("Congratulations!"),
+                                 pos == 1 ? _("Your score is the best!") :
 				 _("Your score has made the top ten."));
       games_scores_dialog_set_message (GAMES_SCORES_DIALOG (scoresdialog),
 				       message);
